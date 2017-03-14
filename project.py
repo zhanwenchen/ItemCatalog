c11833d4bac7d961ca2a1b40d276bc0cddeb452c
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2017-03-14 16:07:19 -0400

    fixed gplus login error

--- project.py
+++ project.py
@@ -9,6 +9,7 @@ import string
 # IMPORTS FOR THIS STEP
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
+from oauth2client.client import AccessTokenCredentials
 import httplib2
 import json
 from flask import make_response
@@ -46,15 +47,32 @@ def gconnect():
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
+        print
+        print('gconnect: 1. Validation complete. response = %s' % (response))
+        print
         return response
+
+
     # Obtain authorization code
     code = request.data
+
+    print
+    print('gconnect: 2. Obtain authorization code complete. auth_code = %s' % (code))
+    print
 
     try:
         # Upgrade the authorization code into a credentials object
         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
+        # credentials.json()
+        # json.dump(credentials)
+        # print(credentials)
+
+        print
+        print('gconnect: 3. Upgrade the authorization code into a credentials object complete. credentials = %s' % (credentials))
+        print
+
     except FlowExchangeError:
         response = make_response(
             json.dumps('Failed to upgrade the authorization code.'), 401)
@@ -63,6 +81,7 @@ def gconnect():
 
     # Check that the access token is valid.
     access_token = credentials.access_token
+    # print('acess token is %s' % (access_token))
     url = ('https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=%s'
            % access_token)
     h = httplib2.Http()
@@ -73,19 +92,27 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
+    print
+    print('gconnect: 4. Validate access token complete. result = %s' % (result))
+    print
+
+
     # Verify that the access token is used for the intended user.
     gplus_id = credentials.id_token['sub']
 
-    print gplus_id
+    # print gplus_id
 
     print json.dumps(result, sort_keys=True,
                   indent=4, separators=(',', ': '))
-
     if result['sub'] != gplus_id:
         response = make_response(
             json.dumps("Token's user ID doesn't match given user ID."), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
+
+    print
+    print('gconnect: 5. Verify user complete. gplus_id = %s' % (gplus_id))
+    print
 
     # Verify that the access token is valid for this app.
     if result['aud'] != CLIENT_ID:
@@ -95,18 +122,38 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    stored_credentials = login_session.get('credentials')
+    print
+    print('gconnect: 6. Verify app complete.')
+    print
+
+    # Verify that the user is not already logged in.
+    stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
-    if stored_credentials is not None and gplus_id == stored_gplus_id:
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
         response = make_response(json.dumps('Current user is already connected.'),
                                  200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
+    print
+    print('gconnect: 7. Check for existing login complete. \
+        stored_access_token = %s \
+        stored_gplus_id = %s' \
+        % (stored_access_token, stored_gplus_id))
+    print
+
     # Store the access token in the session for later use.
-    # login_session['access_token'] = access_token
-    login_session['credentials'] = credentials
+    login_session['access_token'] = access_token
+    ## TODO: do not store credentials. Only access tokens!
+    # login_session['credentials'] = credentials
     login_session['gplus_id'] = gplus_id
+
+    print
+    print('gconnect: 8. Storing access token complete. \
+            \nlogin_session[access_token] = %s, \
+            \nlogin_session[gplus_id] = %s' \
+            % (login_session['access_token'], login_session['gplus_id']))
+    print
 
     # Get user info
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
@@ -115,9 +162,19 @@ def gconnect():
 
     data = answer.json()
 
+    print
+    print('gconnect: 9. Get user info complete. data = %s')
+    print json.dumps(data, sort_keys=True, indent=4, separators=(',',':'))
+    print
+
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
+
+    print
+    print('gconnect: 10. Store user info in login_session complete. login_session = ')
+    print(login_session)
+    print
 
     output = ''
     output += '<h1>Welcome, '
@@ -130,6 +187,7 @@ def gconnect():
     flash("you are now logged in as %s" % login_session['username'])
     print "done!"
 
+    print('gconnect: 11: output is %s' % (output))
     return output
 
 @app.route('/gdisconnect')
@@ -246,8 +304,6 @@ def deletecategory(category_id):
         return render_template('deletecategory.html', category=categoryToDelete)
 
 # Show a category item
-
-
 @app.route('/category/<int:category_id>/')
 @app.route('/category/<int:category_id>/item/')
 def showItems(category_id):
@@ -257,7 +313,7 @@ def showItems(category_id):
     return render_template('item.html', items=items, category=category)
 
 
-# Create a new item item
+# Create a new item
 @app.route('/category/<int:category_id>/item/new/', methods=['GET', 'POST'])
 def newItem(category_id):
     if 'username' not in login_session:
@@ -271,11 +327,9 @@ def newItem(category_id):
         flash('New Menu %s Item Successfully Created' % (newItem.name))
         return redirect(url_for('showItems', category_id=category_id))
     else:
-        return render_template('newitemitem.html', category_id=category_id)
+        return render_template('newitem.html', category_id=category_id)
 
-# Edit a item item
-
-
+# Edit a item
 @app.route('/category/<int:category_id>/item/<int:item_id>/edit', methods=['GET', 'POST'])
 def editItem(category_id, item_id):
     if 'username' not in login_session:
