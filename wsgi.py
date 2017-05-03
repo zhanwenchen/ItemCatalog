501ae94a3161cb2b0b58f4283e81868174ce7c38
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2017-05-03 13:00:11 -0400

    yet more paths

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(0, "/var/www/ItemCatalog")
-sys.path.insert(0, "/home/ubuntu/.virtualenvs/catalog")
+sys.path.insert(0, "/home/ubuntu/.virtualenvs/catalog/lib/python2.7/site-packages")
+print(sys.path)
 
 from project import app
 application = app
